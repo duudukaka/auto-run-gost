76e4804d5f8eab695facecbfb039c09eb8ae2e77
leekunha <34993508+leekunha@users.noreply.github.com>
2020-10-05 11:10:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
+sudo su
 yum update -y&&yum install gunzip wget axel -y
 apt update -y&&apt install gunzip wget axel -y
-
+gostfile="/bin/gost"
 mkdir -p /home/gost
 cd /tmp/
-axel -k https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz -o /tmp/gost.gz
+gost
+if [ $? -eq 0 ]; then
+   echo 'gost installed!'
+elseif [ ! -a "$gostfile" ]; then
+ axel -k https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz -o /tmp/gost.gz
 gunzip /tmp/gost.gz
 chmod 755 gost-linux-amd64
 mv gost-linux-amd64 /bin/gost
+ fi
+
 cat > /lib/systemd/system/gost-serv.service <<EOF
 [Unit]
 Description=gost
