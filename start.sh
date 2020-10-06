ec1038fd197a3dcef041f358df28c9837587d6f7
leekunha <34993508+leekunha@users.noreply.github.com>
2020-10-06 13:26:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,6 +17,7 @@ gost >/dev/null 2>&1
 if [ $? -eq 0 ]; then
    echo 'gost installed!'
 elif [ ! -a "$gostfile" ]; then
+cd /tmp
  wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64-2.11.1.gz -O /tmp/gost.gz
 gunzip /tmp/gost.gz
 chmod 755 gost-linux-amd64
